4ed7df921641b5f825900a24fa7606a5fb8d581e
AntoNieva <35549526+AntoNieva@users.noreply.github.com>
2018-02-09 16:33:03 +0800

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -1,8 +1,6 @@
 package com.example.acme.csci3130_demo;
 
 public class Validator {
-
-    public static int GRADE_SCORE =0 ;
     private final String[] passwordStrong={"very weak","weak","medium","strong","very strong"};
 
     private String validateResultStr;
@@ -19,50 +17,13 @@ public class Validator {
 
     public String validatePassword(String passwordStr) {
 
-        String regexZ = "\\d*";
-        String regexS = "[a-zA-Z]+";
-        String regexT = "\\W+$";
-        String regexZT = "\\D*";
-        String regexST = "[\\d\\W]*";
-        String regexZS = "\\w*";
-        String regexZST = "[\\w\\W]*";
-
         if (passwordStr != null) {
             if (passwordStr.trim().equalsIgnoreCase("password")) {
                 setValidateResultStr("The password cannot be password");
             } else if (passwordStr.trim().length() < 8) {
                 setValidateResultStr("The password lenght cannot be at least 8 characters long");
-            }else if (passwordStr.matches(regexZ)) {
-                GRADE_SCORE = 20;
-                return passwordStrong[0];
-            }else
-            if (passwordStr.matches(regexS)) {
-                GRADE_SCORE = 20;
-                return passwordStrong[0];
-            }else
-            if (passwordStr.matches(regexT)) {
-                GRADE_SCORE = 50;
-                return passwordStrong[1];
-            }else if (passwordStr.matches(regexZS )&& passwordStr.length()<12){
-                GRADE_SCORE = 50;
-                return passwordStrong[1];
-            }else
-            if (passwordStr.matches(regexZT)) {
-                GRADE_SCORE = 80;
-                return passwordStrong[3];
-            }else
-            if (passwordStr.matches(regexST)) {
-                GRADE_SCORE = 60;
-                return passwordStrong[2];
-            }else
-            if (passwordStr.matches(regexZS) && passwordStr.length()>=12) {
-                GRADE_SCORE = 60;
-                return passwordStrong[2];
-            }else
-            if (passwordStr.matches(regexZST)) {
-                GRADE_SCORE = 90;
-                return passwordStrong[4];
             }
+           
             return getValidateResultStr();
         }
 
